975309cc4f729a34985c15f3834c967f3f943d2f
nsbajaj <nitish111@hotmail.com>
2020-05-17 14:26:42 -0400

    Updated Show component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app" class="mx-auto">
-    <div id="nav">
+    <div id="nav" class="row">
       <Navbar @search="searchShow"></Navbar>
       <!-- <router-link to="/show">Show</router-link> | -->
       <!-- <router-link to="/">Home</router-link> |
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,9 +3,10 @@
     <h1>Shows: </h1>
     <ul class="list-unstyled" v-for="(item, index) in shows" :key="item.id" :data-index="index">
       <li class="media">
-        <img v-if="item.show.image" :src="item.show.image.medium" class="mr-3" :alt="item.show.name">
-        <img v-else src="https://lightwidget.com/wp-content/uploads/2018/05/local-file-not-found-295x300.png" class="mr-3" :alt="item.show.name">
-
+        <router-link :to="{ name: 'Show', params: { show: item } }">
+          <img v-if="item.show.image" :src="item.show.image.medium" class="mr-3 img-fluid rounded" :alt="item.show.name">
+          <img v-else src="https://lightwidget.com/wp-content/uploads/2018/05/local-file-not-found-295x300.png" class="mr-3" :alt="item.show.name">
+        </router-link>
         <div class="media-body">
           <router-link :to="{ name: 'Show', params: { show: item } }">
             <!-- <h5 v-if="item.show.image">{{ item.show.image.medium }}</h5> -->
