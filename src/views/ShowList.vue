07a59e4c0b4200ee1849c10989456b37a2cf8a1e
nsbajaj <nitish111@hotmail.com>
2020-05-29 00:10:43 -0400

    Genre filter added

--- src/views/ShowList.vue
+++ src/views/ShowList.vue
@@ -1,61 +1,112 @@
 <template>
   <div class="home row mx-2">
     <div class="col-2">
-      <!-- <ShowsFilter @statusFilter="checkboxFilter"></ShowsFilter> -->
+      <ShowsFilter
+        v-if="shows"
+        :genres="genres"
+        @genresFilter="setSelectedGenres"
+      ></ShowsFilter>
     </div>
     <div class="col-10">
       <h1>Shows:</h1>
       <!-- Shows -->
-      <ul class="list-unstyled">
-        <li
-          class="media my-4"
-          v-for="(item, index) in shows"
-          :key="item.id"
-          :data-index="index"
-        >
-          <router-link :to="{ name: 'Show', params: { id: item.id } }">
-            <img
-              v-if="item.image"
-              :src="item.image.medium"
-              class="mr-3 img-fluid rounded"
-              :alt="item.name"
-            />
-            <img
-              v-else
-              src="https://lightwidget.com/wp-content/uploads/2018/05/local-file-not-found-295x300.png"
-              class="mr-3"
-              :alt="item.name"
-            />
-          </router-link>
-          <div class="media-body">
+      <span v-if="filteredShows.length>0">
+        <ul class="list-unstyled">
+          <li
+            class="media my-4"
+            v-for="(item, index) in filteredShows"
+            :key="item.id"
+            :data-index="index"
+          >
             <router-link :to="{ name: 'Show', params: { id: item.id } }">
-              <h5 class="mt-0 mb-1">{{ item.name }}</h5>
+              <img
+                v-if="item.image"
+                :src="item.image.medium"
+                class="mr-3 img-fluid rounded"
+                :alt="item.name"
+              />
+              <img
+                v-else
+                src="https://lightwidget.com/wp-content/uploads/2018/05/local-file-not-found-295x300.png"
+                class="mr-3"
+                :alt="item.name"
+              />
             </router-link>
-            Description: {{ item.summary | stripHTML }}
-          </div>
-        </li>
-      </ul>
+            <div class="media-body">
+              <router-link :to="{ name: 'Show', params: { id: item.id } }">
+                <h5 class="mt-0 mb-1">{{ item.name }}</h5>
+              </router-link>
+              Description: {{ item.summary | stripHTML }}
+              <br />
+              <br />
+              <span v-for="(genre, index) in item.genres" :key="index">
+                {{ genre }}
+              </span>
+            </div>
+          </li>
+        </ul>
+      </span>
+      <span v-else>
+        <ul class="list-unstyled">
+          <li
+            class="media my-4"
+            v-for="(item, index) in shows"
+            :key="item.id"
+            :data-index="index"
+          >
+            <router-link :to="{ name: 'Show', params: { id: item.id } }">
+              <img
+                v-if="item.image"
+                :src="item.image.medium"
+                class="mr-3 img-fluid rounded"
+                :alt="item.name"
+              />
+              <img
+                v-else
+                src="https://lightwidget.com/wp-content/uploads/2018/05/local-file-not-found-295x300.png"
+                class="mr-3"
+                :alt="item.name"
+              />
+            </router-link>
+            <div class="media-body">
+              <router-link :to="{ name: 'Show', params: { id: item.id } }">
+                <h5 class="mt-0 mb-1">{{ item.name }}</h5>
+              </router-link>
+              Description: {{ item.summary | stripHTML }}
+              <br />
+              <br />
+              <span v-for="(genre, index) in item.genres" :key="index">
+                {{ genre }}
+              </span>
+            </div>
+          </li>
+        </ul>
+      </span>
     </div>
   </div>
 </template>
 
 <script>
+import ShowsFilter from "./ShowsFilter.vue";
 
 export default {
   name: "ShowList",
   data() {
     return {
-      shows: null
+      shows: null,
+      genres: [],
+      filteredShows: []
+      // query: [
+      //   "Drama"
+      // ]
     };
   },
   components: {
-    
+    ShowsFilter,
   },
-  props: [
-    
-  ],
+  props: [],
   created() {
-      this.getAllShows();
+    this.getAllShows();
   },
   methods: {
     getAllShows: function() {
@@ -69,11 +120,38 @@ export default {
         .then((response) => response.json())
         .then((data) => {
           this.shows = data;
+
+          //Getting Genres
+          this.filterGenres();
         })
         .catch(function(error) {
           console.log(error);
         });
-    }   
-  }
+    },
+    filterGenres: function() {
+      if (this.shows) {
+        for (let i = 0; i < this.shows.length; i++) {
+          for (let j = 0; j < this.shows[i].genres.length; j++) {
+            if (!this.genres.includes(this.shows[i].genres[j])) {
+              this.genres.push(this.shows[i].genres[j]);
+            }
+          }
+        }
+        this.genres.sort();
+      }
+    },
+    setSelectedGenres: function(data) {
+      this.filteredShows = [];
+      if(data.length > 0){
+       for(let i = 0; i < this.shows.length; i++){
+          let show = this.shows[i];
+          let result = show.genres.filter(genre => data.includes(genre));
+          if(result.length > 0){
+            this.filteredShows.push(show);
+          }
+       }
+      }
+    },
+  },
 };
 </script>
--- src/views/ShowsFilter.vue
+++ src/views/ShowsFilter.vue
@@ -1,13 +1,20 @@
 <template>
     <div class="card my-2">
-        <h5 class="card-header">Filter by status:</h5>
+        <!-- <h5 class="card-header">Filter by status:</h5>
         <div class="card-body">
             <div class="form-check" v-for="item in showStatus" :key="item.status">
                   <input @change="$emit('statusFilter', checkedStatus)" class="form-check-input" type="checkbox"
                   :id="item.status" :value="item.status" v-model="checkedStatus">
                   {{ item.status }}
             </div>
-            <!-- <span>Checked names: {{ checkedStatus }}</span> -->
+        </div> -->
+        <h5 class="card-header">Filter by genres:</h5>
+        <div class="card-body">
+            <div class="form-check" v-for="item in genres" :key="item">
+                <input @change="$emit('genresFilter', checkedGenres)" class="form-check-input" type="checkbox"
+                :id="item" :value="item" v-model="checkedGenres">
+                {{ item }}
+            </div>
         </div>
     </div>
 </template>
@@ -23,8 +30,15 @@ export default {
                 { status: 'To Be Determined' },
                 { status: 'In Development' }
             ],
-            checkedStatus: []
+            checkedStatus: [],
+            checkedGenres: []
         };
-    }
+    },
+    props: [
+        'genres'
+    ],
+    created() {
+        
+    },
 }
 </script>
